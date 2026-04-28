46c44a8ce5a82f00b2bc5e775881aeba48c7879e
nantr0nic <andy15givingup@gmail.com>
2026-04-28 16:02:27 -0700

    Did the web server lab from the networking textbook

---
+++ network-programming/WebServerLab/webserver.py
@@ -0,0 +1,55 @@
+import sys
+from socket import *
+
+serverPort = 80
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# line below 'releases' the port immediately after script closes
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+try:
+    serverSocket.bind(('', serverPort))
+    serverSocket.listen(1)
+    print(f"Web server up and listening on port {serverPort}.")
+    print("Press CTRL + C to shut down server.")
+
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        print(f"Connection from: {addr}")
+
+        try:
+            message = connectionSocket.recv(1024).decode()
+            # this guards against empty requests
+            if not message:
+                connectionSocket.close()
+                continue
+
+            filename = message.split()[1]
+            f = open(filename[1:])
+            outputdata = f.readlines()
+
+            # Send the HTTP Status Line (success in this case) and headers
+            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+            connectionSocket.send("Content-Type: text/html\r\n".encode())
+            # Send one blank line to signal the end of the headers
+            connectionSocket.send("\r\n".encode())
+
+            # Send the content of the requested file to the client
+            for line in outputdata:
+                connectionSocket.send(line.encode())
+
+            # Final CRLF closes the message
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()
+            print(f"Successfully served {filename}")
+
+        except IOError:
+            # Send response message for file not found
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+            connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode())
+            connectionSocket.close()
+            print(f"File {filename} not found.")
+
+except KeyboardInterrupt:
+    print("\nShutting down the server...")
+    serverSocket.close()
+    sys.exit()
